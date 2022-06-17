d4622b0fb7e97d3bc9f06458f9fe45f1af1affbc
Matvey309 <mat-bazhanov02@mail.ru>
2022-06-17 17:14:26 +0300

    A handman

---
+++ handman.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+int main()
+{
+    const int TRY = 10;
+    // вектор со словами
+    vector <string> words;
+    words.push_back("hello");
+    words.push_back("word");
+    words.push_back("matvey");
+    // вектор для проверок
+    vector <string> checkWord;
+    string hightWord;
+    cout << "Привет это игра висилица" << endl;
+    cout << "У тебя 10 попыток !" << endl;
+    // for(int i = 0; i < words[0].size(); i++)
+    // {
+    // }
+    checkWord.push_back()
+    cout << endl; 
+    return 0;
+}
+// clear && g++ handman.cpp -o handman.out && ./handman.out
