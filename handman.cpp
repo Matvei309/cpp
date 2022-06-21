6f3cc857f71217f1244ca0c252edf27a722434c7
Matvey309 <mat-bazhanov02@mail.ru>
2022-06-21 12:17:22 +0300

    A hangman

--- handman.cpp
+++ handman.cpp
@@ -5,52 +5,70 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
-/*
-
-[+] Создать подборку слов
-[] Выбрать из подборки одно слово. загадать его
-[] Если игрок еще не превысил лимит ошибок. но и не разгадал это слово
-[]      Сообщить игроку. сколько ошибок он еще имеет право допустить Показать игроку буквы. которые он уже угадал
-[]      Показать игроку. какую часть загаданного слова он уже открыл Получить от игрока следующий вариант буквы
-[]      Если игрок предложит букву. которую он уже угадал
-[]            Получить вариант игрока
-
-*/
 
 int main()
 {
     int tries = 10;
+    char checkChar;
     // вектор со словами
     vector <string> words;
     words.push_back("hello");
     words.push_back("word");
     words.push_back("matvey");
-
+    words.push_back("algorithm");
+    
     // string baseWord = ? получить слчайное слово с помощью `shuffle`
     // string wordForUser = ? создать строку каждый символ которой - черта (кол-во должно совпадать с длиной baseWord)
-
-    string baseWord = words[0];
-    cout << baseWord << endl;
-
-    string wordForUser = string(baseWord.length(), '_');
-    cout << wordForUser << endl;
-
-    cout << baseWord.find('h') << endl;
-    // как заменить в wordForUser '-' на буквы ?
     
-    // // вектор для проверок
-    // vector <string> checkWord;
-    // string hightWord;
-    // cout << "Привет это игра висилица" << endl;
-    // cout << "У тебя 10 попыток !" << endl;
-    // // for(int i = 0; i < words[0].size(); i++)
-    // // {
-    // // }
-    // checkWord.push_back()
+    // случайный элемент 
+    srand(time(0));
+    int randNum = rand() % words.size();
+    random_shuffle(words.begin(), words.end());
 
+    // Вступление
+    cout << "START" << endl;
+    cout << "Привет, это игра висилца" << endl;
+    cout << "В ней ты должен отгатать слово" << endl;
+    cout << "У тебя "<< tries << " попыток" << endl;
+    cout << endl << endl;
+    
+    // хранение слов
+    string baseWord = words[randNum];
+    string wordForUser = string(baseWord.length(), '_');
+
+    cout << wordForUser << endl;
+    // игровой цикл
+    while (tries > 0)
+    {
+        if (baseWord == wordForUser)
+            break;
+        cout << "Буква -> ";
+        cin >> checkChar;
+        int findIndex = baseWord.find(checkChar);
+        
+        if (findIndex <= baseWord.length())
+        {
+            wordForUser[findIndex] = baseWord[findIndex];
+            for (int i = findIndex + 1; i < baseWord.length(); i++)
+            {
+                if (checkChar == baseWord[i])
+                    wordForUser[i] = baseWord[i];
+            }
+        cout << wordForUser << endl;
+        }
+        
+        else
+        {
+        cout << "Попробуй еще раз" << endl;
+        cout << "У тебя "<< tries << " попыток" << endl;
+        tries -= 1;
+        cout << wordForUser << endl;
+        }
+    }
     cout << endl;
     return 0;
 }
